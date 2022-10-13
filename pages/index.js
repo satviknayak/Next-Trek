3aa39d35849eb54fe6e9617872a6a0fe75b3ea47
Satvik S Nayak <nayaksatvik02@gmail.com>
2022-10-13 23:16:59 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
           <h1 className="font-comforter font-semibold text-[2.5rem] mb-[20px] px-[1rem]">Travel Destinations</h1>
           <p className="mb-[45px] px-[1rem]">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industrys standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
@@ -42,7 +42,7 @@ export default function Home() {
           <h1 className="font-comforter font-semibold text-[2.5rem] mb-[20px] px-[1rem]">Travel Blogs</h1>
           <p className="mb-[45px] px-[1rem]">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industrys standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
