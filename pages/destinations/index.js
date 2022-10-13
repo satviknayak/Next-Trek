bc4913544c8e40fd346df377e84c8d698766ea5f
K SHRAVAN <86780079+dark-coder-144@users.noreply.github.com>
2022-10-13 22:18:58 +0530

    destination page and blog page ui added

--- pages/destinations/index.js
+++ pages/destinations/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import Image from 'next/image'
 export default function index() {
   return (
-    <div></div>
-  )
+   <div className='flex flex-col sm:flex-row w-full h-fit mt-[4rem] pb-20'>
+      <div id="card">
+        <h2 class="text-center font-serif  uppercase text-4xl xl:text-5xl pb-20 pt-10">Famous Destinations</h2>
+        <div class=" container w-50 lg:w-3/5 mx-auto flex flex-wrap">
+        <div class="flex flex-col md:flex-row overflow-hidden bg-white rounded-lg shadow-xl  mt-4 mx-20 items-center">
+          <div class="h-auto w-auto">
+            <Image  width={450} height={300} className="object-center" src={'/../public/dest-illustration.png'} />
+          </div>
+          <div class="w-auto h-auto py-4 px-6 text-gray-800 flex flex-col justify-between">
+          <h3 class="font-semibold text-lg leading-tight truncate">Destination Name</h3>
+          <p class="mt-2 pb-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus debitis, error deserunt fugiat veritatis magni aperiam assumenda recusanda 
+          </p>
+          <button class=' bg-white hover:bg-black hover:text-white border border-solid border-grey w-1/3 py-2'>Read More</button>
+          </div>
+        </div>
+        </div>
+      </div>
+   </div>
+  );
 }
--- pages/travel-blogs/index.js
+++ pages/travel-blogs/index.js
@@ -1,7 +1,23 @@
 import React from 'react'
-
+import Image from 'next/image'
 export default function index() {
   return (
-    <div></div>
-  )
+   <div className='flex flex-col sm:flex-row w-full h-fit mt-[4rem] pb-20'>
+    <div>
+    <h2 class="text-center font-serif  uppercase text-4xl xl:text-5xl pb-20 pt-10">Travel Blogs</h2>
+    <div class="container w-40 lg:w-3/5 p-3 mx-auto flex flex-wrap"> 
+      <div class="flex flex-col lg:flex-row-reverse rounded overflow-hidden h-auto  border shadow">
+        <Image  width={450} height={250} className="object-center block h-auto w-full lg:w-48 flex-none bg-cover" src={'/../public/blog.png'} />
+        <div class="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div class="text-black font-bold text-xl mb-2 leading-tight">My Travel Experience</div>
+        <p class="mt-2 pb-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus debitis, error deserunt fugiat veritatis magni aperiam assumenda recusanda
+          </p>
+          <button class=' bg-white hover:bg-black hover:text-white border border-solid border-grey w-1/3 py-2'>Read More</button>
+      </div>
+      </div>
+    </div>
+    </div>
+   </div>
+  );
 }
